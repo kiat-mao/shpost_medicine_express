00077fb689e2a5ad51a7251ba2f1332817c95c44
yyy <kiat.mao@gmail.com>
2022-09-21 13:47:14 +0800

    update migrate

--- db/migrate/20220727012156_change_column_package.rb
+++ db/migrate/20220727012156_change_column_package.rb
@@ -1,5 +1,11 @@
 class ChangeColumnPackage < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    change_column :packages, :packed_at, :datetime
+  end
+
+  def down
     change_column :packages, :packed_at, :datetime
   end
 end
+
+
