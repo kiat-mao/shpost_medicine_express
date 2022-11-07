ed3be007e82d7e0a34c8e3d297b500559166a1ed
Tony.Gao <kzcatvv@gmail.com>
2022-11-07 15:59:00 +0800

    modify

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_26_024638) do
+ActiveRecord::Schema.define(version: 2022_11_03_065535) do
 
   create_table "bags", force: :cascade do |t|
     t.string "bag_no"
@@ -126,8 +126,8 @@ ActiveRecord::Schema.define(version: 2022_10_26_024638) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "order_list"
-    t.string "bag_list"
+    t.string "order_list", limit: 2000
+    t.string "bag_list", limit: 2000
     t.index ["express_no"], name: "index_packages_on_express_no"
     t.index ["package_no"], name: "index_packages_on_package_no"
     t.index ["user_id"], name: "index_packages_on_user_id"
