368c2e1fba2a8f06e677b736089d82fa4afdd7e1
kiat <kiat.mao@gmail.com>
2022-11-14 14:44:11 +0800

    fix bug

--- db/migrate/20221110072943_rename_feight_to_freight_on_orders.rb
+++ db/migrate/20221110072943_rename_feight_to_freight_on_orders.rb
@@ -2,7 +2,4 @@ class RenameFeightToFreightOnOrders < ActiveRecord::Migration[6.0]
   def change
     rename_column :Orders, :feight, :freight
   end
-  def change
-    rename_column :Orders, :freight, :feight
-  end
 end
