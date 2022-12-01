250789b5323aaefbcf657aa3014694433cd119fd
yyy <kiat.mao@gmail.com>
2022-12-01 10:23:26 +0800

    update add_weight_to_orders.rb

--- db/migrate/20221111013825_add_weight_to_orders.rb
+++ db/migrate/20221111013825_add_weight_to_orders.rb
@@ -1,7 +1,7 @@
 class AddWeightToOrders < ActiveRecord::Migration[6.0]
   def change
-    add_column :orders, :weight, :double
-    add_column :orders, :volume, :double
-    add_column :orders, :price, :double
+    # add_column :orders, :weight, :double
+    # add_column :orders, :volume, :double
+    # add_column :orders, :price, :double
   end
 end
