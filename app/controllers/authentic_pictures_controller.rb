a15eac7edcb95773aabb49c7e09326f03992d444
yyy <kiat.mao@gmail.com>
2024-09-19 14:04:28 +0800

    result order by posting_date

--- app/controllers/authentic_pictures_controller.rb
+++ app/controllers/authentic_pictures_controller.rb
@@ -2,7 +2,11 @@ class AuthenticPicturesController < ApplicationController
   load_and_authorize_resource :authentic_picture
 
   def authentic_pictures_report
-  	@results = AuthenticPicture.where("posting_date >= ? and posting_date < ?", Date.today-29.days, Date.tomorrow).group("Date(posting_date)").group(:status).count
+  	if RailsEnv.is_oracle?
+	  	@results = AuthenticPicture.where("posting_date >= ? and posting_date < ?", Date.today-29.days, Date.tomorrow).order("to_char(posting_date, 'YYYY-MM-DD')").group("to_char(posting_date, 'YYYY-MM-DD')").group(:status).count
+	  else
+	  	@results = AuthenticPicture.where("posting_date >= ? and posting_date < ?", Date.today-29.days, Date.tomorrow).order("Date(posting_date)").group("Date(posting_date)").group(:status).count
+	  end
   	@results_hj = AuthenticPicture.where("posting_date >= ? and posting_date < ?", Date.today-29.days, Date.tomorrow).group(:status).count
   end
 end
